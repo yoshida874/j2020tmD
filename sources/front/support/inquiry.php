7f92927e74ef6ca5fc341fb66f63f11d62b1f194
yoshida874 <ud8mnzdd@gmail.com>
2020-05-29 15:39:00 +0900

    supportのsmarty対応

--- sources/front/support/inquiry.php
+++ sources/front/support/inquiry.php
@@ -1,36 +1,11 @@
 ﻿<?php
 //問い合わせ画面
 
+require_once("inc_base.php");
+require_once($CMS_COMMON_INCLUDE_DIR . "libs.php");
+require_once("inc_smarty.php");
+
+//Smartyを使用した表示(テンプレートファイルの指定)
+$smarty->display('front/support/inquiry.tmpl');
 ?>
 
-<!DOCTYPE html>
-<html lang="ja">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-
-<body>
-    <h1>お問い合わせ</h1>
-    <P>お問い合わせの種類・内容・メールアドレスをご入力の上、「送信」ボタンをクリックして下さい</P>
-    <p>お問い合わせの種類<br>
-    <select name=”inquiry”>
-        <option value="">選択してください</option>
-        <option value=”login”>ログインできない</option>
-        <option value=”signup”>会員登録について</option>
-    </select>
-    </p>
-    <p>お問い合わせ内容<br>
-        <textarea name="Content" cols="100" rows="10"></textarea>
-    </p>
-    <p>
-        メールアドレス<br>
-        <input type="text" size="50">
-    </p>
-    <div align="center">
-        <input type="button" value="送信">
-    </div>
-</body>
-</html>
