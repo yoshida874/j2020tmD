74f119bab0b0fc5e6b341a9a1c9401ba8614ba23
RimlTempest <riml.slime@gmail.com>
2020-05-29 16:00:37 +0900

    追加されてたプロフィールのテンプレートにphpファイルの紐付け

---
+++ sources/npo/profile.php
@@ -0,0 +1,8 @@
+<?php
+
+require_once("inc_base.php");
+require_once($CMS_COMMON_INCLUDE_DIR . "libs.php");
+require_once("inc_smarty.php");
+
+//Smartyを使用した表示(テンプレートファイルの指定)
+$smarty->display('npo/profile.tmpl');
