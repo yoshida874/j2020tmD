d509f41b9154501c0a9de3c1b2479303dedf61c9
Bridgehead <ase18410220@wiz.ac.jp>
2020-06-05 13:25:12 +0900

    pageFlagをsmarty用に書き換え

--- sources/admin/adminEdit.php
+++ sources/admin/adminEdit.php
@@ -2,6 +2,8 @@
 require_once("inc_base.php");
 require_once($CMS_COMMON_INCLUDE_DIR . "libs.php");
 require_once("inc_smarty.php");
+//変数宣言
+$smarty->assign("pageFlag","0");
 //Smartyを使用した表示(テンプレートファイルの指定)
 $smarty->display('admin/adminEdit.tmpl');
 ?>
