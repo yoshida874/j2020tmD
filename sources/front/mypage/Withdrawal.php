11c6224dfb03bebbb85ac5f86762f91d43a4f1fb
RimlTempest <riml.slime@gmail.com>
2020-06-05 15:22:30 +0900

    php側にもheader内容など適用

--- sources/front/mypage/Withdrawal.php
+++ sources/front/mypage/Withdrawal.php
@@ -5,6 +5,15 @@ require_once("inc_base.php");
 require_once($CMS_COMMON_INCLUDE_DIR . "libs.php");
 require_once("inc_smarty.php");
 
+$header_items = array(
+    'マイページ' => './mypage/userprofile.php',
+    '新着イベント' => './event_search.php',
+    'イベント編集' => './event_detail.php',
+    'イベント作成' => './event_create.php',
+    'ログイン' => './login.php'
+);
+
+$smarty->assign('page', $header_items);
+
 //Smartyを使用した表示(テンプレートファイルの指定)
 $smarty->display('front/mypage/Withdrawal.tmpl');
-?>
--- sources/front/mypage/userprofile.php
+++ sources/front/mypage/userprofile.php
@@ -18,6 +18,16 @@ $smarty->assign('ageOfChild', '子供の年齢');
 $smarty->assign('genderOfChild', '子供の性別');
 $smarty->assign('introduce', '自己紹介文');
 
+$header_items = array(
+    'マイページ' => './mypage/userprofile.php',
+    '新着イベント' => './event_search.php',
+    'イベント編集' => './event_detail.php',
+    'イベント作成' => './event_create.php',
+    'ログイン' => './login.php'
+);
+
+$smarty->assign('page', $header_items);
+
 
 //Smartyを使用した表示(テンプレートファイルの指定)
 $smarty->display('front/mypage/userprofile.tmpl');
