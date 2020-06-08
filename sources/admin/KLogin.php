a740d4a30f155f680f45ec92de4753dc218a86e1
Bridgehead <ase18410220@wiz.ac.jp>
2020-06-08 11:42:17 +0900

    admin header,footer追加修正

--- sources/admin/KLogin.php
+++ sources/admin/KLogin.php
@@ -2,6 +2,9 @@
 require_once("inc_base.php");
 require_once($CMS_COMMON_INCLUDE_DIR . "libs.php");
 require_once("inc_smarty.php");
+require_once("inc_header.php");
+
+$smarty->assign('page', $header_items);
 //Smartyを使用した表示(テンプレートファイルの指定)
 $smarty->display('admin/KLogin.tmpl');
 ?>
