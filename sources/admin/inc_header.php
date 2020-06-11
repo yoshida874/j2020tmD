85d6963091136cfabe8fdbea4dc6924ad0d609e5
yoshida874 <ud8mnzdd@gmail.com>
2020-06-11 15:49:16 +0900

    admin側の命名変更

--- sources/admin/inc_header.php
+++ sources/admin/inc_header.php
@@ -8,12 +8,12 @@ $root_path = '/~j2020tmD';
 $sub_dir = '/sources/admin';
 
 $header_items = array(
-    '管理者HOME' => $root_path . $sub_dir . '/KHome.php',
+    '管理者HOME' => $root_path . $sub_dir . '/home.php',
     '管理者一覧' => $root_path . $sub_dir . '/adminList.php',
     'ユーザー一覧' => $root_path . $sub_dir . '/userList.php',
     'イベント一覧' => $root_path . $sub_dir . '/Event_List.php',
     '団体一覧' => $root_path . $sub_dir . '/Dantai_List.php',
     'お問い合わせ一覧' => $root_path . $sub_dir . '/inquiryList.php',
     'ブラックリスト' => $root_path . $sub_dir . '/Blacklist_List.php',
-    'ログイン' => $root_path . $sub_dir . '/KLogin.php'
+    'ログイン' => $root_path . $sub_dir . '/login.php'
 );
