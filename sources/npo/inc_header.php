019c6e00aedf080b0ee55c56aebb52adb62525a8
yoshida874 <ud8mnzdd@gmail.com>
2020-06-11 15:08:40 +0900

    NPOヘッダー参照変更

--- sources/npo/inc_header.php
+++ sources/npo/inc_header.php
@@ -9,8 +9,8 @@ $sub_dir = '/sources/npo';
 
 $header_items = array(
     'プロフィール' => $root_path . $sub_dir . '/profile_create.php',
-    '管理者' => $root_path . $sub_dir . '/admin_us.php',
-    'お問い合わせ' => $root_path . $sub_dir . '/contact_us.php',
+    '管理者' => $root_path . $sub_dir . '/admin_list.php',
+    'お問い合わせ' => $root_path . $sub_dir . '/contact_list.php',
     'イベント作成' => $root_path . $sub_dir . '/event_create.php',
-    'イベント一覧' => $root_path . $sub_dir . '/event_us.php'
+    'イベント一覧' => $root_path . $sub_dir . '/event_list.php'
 );
