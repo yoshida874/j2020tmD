0547efb4ef103129d3fc17f53892e9d3bf9e97a1
yoshida874 <ud8mnzdd@gmail.com>
2020-06-25 13:56:25 +0900

    メール送信処理

--- sources/npo/Contact_Detail.php
+++ sources/npo/Contact_Detail.php
@@ -4,6 +4,7 @@ require_once("inc_base.php");
 require_once($CMS_COMMON_INCLUDE_DIR . "libs.php");
 require_once("inc_smarty.php");
 require_once("inc_header.php");
+require_once("inc_mail.php");
 
 $smarty->assign('page', $header_items);
 
@@ -86,6 +87,34 @@ else{
 
 //--------------------------------------------------------------------------------------
 /*!
+@brief	エラー存在のアサイン
+@return	なし
+*/
+//--------------------------------------------------------------------------------------
+function assign_err_flag(){
+	//$smartyをグローバル宣言（必須）
+	global $smarty;
+	global $err_flag;
+	$str = '';
+	switch($err_flag){
+		case 1:
+		$str =<<<END_BLOCK
+
+<p class="red">入力エラーがあります。各項目のエラーを確認してください。</p>
+END_BLOCK;
+		break;
+		case 2:
+		$str =<<<END_BLOCK
+
+<p class="red">更新に失敗しました。サポートを確認下さい。</p>
+END_BLOCK;
+		break;
+	}
+	$smarty->assign('err_flag',$str);
+}
+
+//--------------------------------------------------------------------------------------
+/*!
 @brief	パラメータのチェック
 @return	エラーの場合はfalseを返す
 */
@@ -95,6 +124,9 @@ function paramchk(){
 	$retflg = true;
 	/// ユーザー名の存在と空白チェック
 	if(ccontentsutil::chkset_err_field($err_array,'user_name','ユーザー名','isset_nl')){
+		$retflg = false;
+	}
+	if(ccontentsutil::chkset_err_field($err_array,'user_id','ユーザーID','isset_nl')){
 		$retflg = false;
 	}
 	return $retflg;
@@ -114,6 +146,38 @@ function assign_inquiry_id(){
 }
 
 
+//--------------------------------------------------------------------------------------
+/*!
+@brief  メール送信後自分自身を再読み込みする。
+@return なし
+*/
+//--------------------------------------------------------------------------------------
+function regist(){
+    //必要に応じてここでDB保存処理
+
+	$to_mail = 'ud8mnzdd@gmail.com';
+    //これよりメール送信
+    global $to_member_examinee_template;
+    //ユーザー向け
+    $Subject = '問い合わせありがとうございます';
+    $BaseMessage = $to_member_examinee_template;
+	//$BaseMessage = str_replace('<!-- to_mail -->',$_POST['reply_address'],$BaseMessage);
+    $BaseMessage = str_replace('<!-- to_mail -->',$to_mail,$BaseMessage);
+
+    $Message = $BaseMessage;
+	//$Headers = "From: " . $_POST['from_mail'] . "\r\n";
+	$Headers = "From: " . 'yoshida.taiki@outlook.jp' . "\r\n";
+    $Headers .= "Content-Type: text/plain; charset=ISO-2022-JP";
+    $Message .= "\r\n";
+    //「from」から「to」に送るメールとして送信する
+	//$To = $_POST['reply_address'];
+	$To = $to_mail;
+    //本番送信
+	mb_send_mail($To, $Subject, $Message, $Headers);
+	
+    cutil::redirect_exit('Contact_List.php');
+}
+
 /////////////////////////////////////////////////////////////////
 /// 関数呼び出しブロック
 /////////////////////////////////////////////////////////////////
@@ -121,6 +185,8 @@ if(!isset($_POST['user_name']))$_POST['user_name'] = '';
 if(!isset($_POST['content']))$_POST['content'] = '';
 if(!isset($_POST['user_id']))$_POST['user_id'] = '';
 if(!isset($_POST['target_date']))$_POST['target_date'] = '';
+if(!isset($_POST['reply_address']))$_POST['reply_address'] = '';
+assign_err_flag();
 assign_inquiry_id();
 $smarty->assign('err_array',$err_array);
 
---
+++ sources/npo/inc_mail.php
@@ -0,0 +1,10 @@
+<?php
+
+$to_member_examinee_template=<<<END_BLOCK
+<!-- to_mail -->様
+このたびはお問い合わせいただき
+ありがとうございました。
+END_BLOCK;
+
+
+?>
