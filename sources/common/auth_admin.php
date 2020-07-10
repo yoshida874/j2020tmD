31c56c82dbd1d0214f4d32658ff9a28c4d65df3e
yoshida874 <ud8mnzdd@gmail.com>
2020-07-10 13:43:54 +0900

    ログインセッション

---
+++ sources/common/auth_admin.php
@@ -0,0 +1,34 @@
+<?php
+/********************************
+
+auth_admin.php
+
+メンバーログイン認証
+認証が必要なページはこのファイルをインクルードする
+すでにlibs.phpがインクルードされている必要がある
+*複数のサイトが同居または管理者管理と混同しないため
+$_SESSIONは多次元配列にする
+
+*********************************/
+session_start();
+if((!isset($_SESSION['j2020tmD_admin']['login_user'])) 
+    || (!isset($_SESSION['j2020tmD_admin']['user_id']))){
+    cutil::redirect_exit("Login_Page.php");
+}
+$admin = new cadmin_user();
+$row = $admin->get_tgt_login(false,$_SESSION['j2020tmD_admin']['login_user']);
+if($row === false || !isset($row['user_id'])){
+    cutil::redirect_exit("Login_Page.php");
+}
+if($row['user_id'] != $_SESSION['j2020tmD_admin']['user_id']){
+    cutil::redirect_exit("Login_Page.php");
+}
+
+function echo_hello_user_name(){
+    if(isset($_SESSION['j2020tmD_admin']['user_name'])){
+        echo $_SESSION['j2020tmD_admin']['user_name'];
+    }
+}
+
+
+?>
