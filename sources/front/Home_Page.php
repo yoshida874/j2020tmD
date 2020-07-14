dde53b2cd5f176101e80ec0f7575b92766b9eef6
Bridgehead <ase18410220@wiz.ac.jp>
2020-07-14 11:51:11 +0900

    front/home,mypage

--- sources/front/Home_Page.php
+++ sources/front/Home_Page.php
@@ -4,9 +4,11 @@ require_once("inc_base.php");
 require_once($CMS_COMMON_INCLUDE_DIR . "libs.php");
 require_once("inc_smarty.php");
 require_once("inc_header.php");
+require_once($CMS_COMMON_INCLUDE_DIR . "auth_user.php");
 
 $smarty->assign('page', $header_items);
 
+var_dump($_SESSION);
 //Smartyを使用した表示(テンプレートファイルの指定)
 $top_path = 'front/';
 $base_name = basename(__FILE__, ".php");
--- sources/front/mypage/Userprofile_Page.php
+++ sources/front/mypage/Userprofile_Page.php
@@ -5,18 +5,33 @@ require_once("inc_base.php");
 require_once($CMS_COMMON_INCLUDE_DIR . "libs.php");
 require_once("inc_smarty.php");
 require_once("inc_header.php");
+require_once($CMS_COMMON_INCLUDE_DIR . "auth_user.php");
 
+function read_data($member_id)
+{
+	//$smartyをグローバル宣言（必須）
+	global $smarty;
+	global $limit;
+	global $rows;
+	global $order;
+	global $page;
+	$obj = new cuser();
+  $rows = $obj->get_tgt_uid(false,$member_id);
+  $smarty->assign('rows', $rows);
+}
+//echo $_SESSION['j2020tmD_user']['user_id'];
+read_data($_SESSION['j2020tmD_user']['user_id']);
 //ユーザープロフィール
-$smarty->assign('userId', 'ユーザID');
-$smarty->assign('name', '名前');
-$smarty->assign('birth', '生年月日');
-$smarty->assign('interest', '趣味');
+$smarty->assign('userId', $rows["user_id"]);
+$smarty->assign('name', $rows["user_name"]);
+$smarty->assign('birth', $rows["birthday"]);
+$smarty->assign('interest', $rows["hobby"]);
 $smarty->assign('numOfFollow', 'フォロー');
 $smarty->assign('numOfFollower', 'フォロワー');
 $smarty->assign('nameOfChild', '子供の名前');
 $smarty->assign('ageOfChild', '子供の年齢');
 $smarty->assign('genderOfChild', '子供の性別');
-$smarty->assign('introduce', '自己紹介文');
+$smarty->assign('introduce', $rows["profile_content"]);
 
 $smarty->assign('page', $header_items);
 
