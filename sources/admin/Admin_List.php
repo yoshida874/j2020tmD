e1210db426eb9462f0b84bd59770c39345e9ab71
yoshida874 <ud8mnzdd@gmail.com>
2020-07-30 11:50:29 +0900

    Admin_LIstが消えていたのでpush

---
+++ sources/admin/Admin_List.php
@@ -0,0 +1,101 @@
+<?php
+require_once("inc_base.php");
+require_once($CMS_COMMON_INCLUDE_DIR . "libs.php");
+require_once("inc_smarty.php");
+require_once("inc_header.php");
+
+$smarty->assign('page', $header_items);
+
+//1ページのリミット
+$limit = 20;
+//ページの設定
+//デフォルトは1
+$page = 1;
+$rows = array();
+$ERR_STR = '';
+$show_mode = '';
+
+//もしページが指定されていたら
+if(isset($_GET['page'])
+    //なおかつ、数字だったら
+    && cutil::is_number($_GET['page'])
+    //なおかつ、0より大きかったら
+    && $_GET['page'] > 0){
+    //パラメータを設定
+    $page = $_GET['page'];
+}
+
+function readdata()
+{
+	global $limit;
+	global $rows;
+	global $order;
+	global $page;
+	$obj = new cadmin_user();
+	$from = ($page - 1) * $limit;
+	$rows = $obj->get_all(false, $from, $limit);
+}
+
+
+readdata();
+
+//--------------------------------------------------------------------------------------
+/*!
+@brief	ページャーのアサイン
+@return	なし
+*/
+//--------------------------------------------------------------------------------------
+function assign_page_block()
+{
+	//$smartyをグローバル宣言（必須）
+	global $smarty;
+	global $limit;
+	global $page;
+	$retstr = '';
+	$obj = new cadmin_user();
+	$allcount = $obj->get_all_count(false);
+	$ctl = new cpager($_SERVER['PHP_SELF'], $allcount, $limit);
+	$smarty->assign('pager_arr', $ctl->get('page', $page));
+}
+
+//--------------------------------------------------------------------------------------
+/*!
+@brief	一覧のアサイン
+@return	なし
+*/
+//--------------------------------------------------------------------------------------
+function assign_ADMIN_user_list()
+{
+	//$smartyをグローバル宣言（必須）
+	global $smarty;
+	global $rows;
+	$smarty->assign('rows', $rows);
+}
+
+//--------------------------------------------------------------------------------------
+/*!
+@brief	URIのアサイン
+@return	なし
+*/
+//--------------------------------------------------------------------------------------
+function assign_tgt_uri(){
+	//$smartyをグローバル宣言（必須）
+	global $smarty;
+	global $page;
+	$smarty->assign('tgt_uri',$_SERVER['PHP_SELF'] . '?page=' . $page);
+}
+
+/////////////////////////////////////////////////////////////////
+/// 関数呼び出しブロック
+/////////////////////////////////////////////////////////////////
+$smarty->assign('ERR_STR', $ERR_STR);
+assign_page_block();
+assign_ADMIN_user_list();
+//assign_tgt_uri();
+
+
+
+//Smartyを使用した表示(テンプレートファイルの指定)
+$top_path = 'admin/';
+$base_name = basename(__FILE__, ".php");
+$smarty->display($top_path . $base_name . '.tmpl');
