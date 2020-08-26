7486f11dfdf0bb63aa44f481fe130e28077a1975
yoshida874 <ud8mnzdd@gmail.com>
2020-08-26 15:52:32 +0900

    profileへのリンクを削除

--- sources/npo/inc_header.php
+++ sources/npo/inc_header.php
@@ -25,7 +25,6 @@ else{
 
 $header_items = array(
     'ホーム' => $root_path . $sub_path . 'Home_Page.php',
-    'プロフィール' => $root_path . $sub_path . 'Profile_Create.php',
     '管理者' => $root_path . $sub_path . 'Admin_List.php',
     'お問い合わせ' => $root_path . $sub_path . 'Inquiry_List.php',
     'イベント作成' => $root_path . $sub_path . 'Event_Create.php',
