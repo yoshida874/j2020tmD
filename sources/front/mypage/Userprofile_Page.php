b433736111628bef87a7e87c1662ebb8ffa56655
yoshida874 <ud8mnzdd@gmail.com>
2020-08-27 00:57:01 +0900

    バグ修正

--- sources/front/mypage/Userprofile_Page.php
+++ sources/front/mypage/Userprofile_Page.php
@@ -7,6 +7,7 @@ require_once("inc_smarty.php");
 require_once("inc_header.php");
 require_once($CMS_COMMON_INCLUDE_DIR . "auth_user.php");
 
+$user = '';
 function read_data_user($member_id)
 {
 	//$smartyをグローバル宣言（必須）
